be2bed46d768c5c17c482a5538e38485078a5c86
Jenhsiang Hsieh (Wren) <wrenhsieh18@gmail.com>
2022-03-08 20:34:41 -0600

    saved

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -7,11 +7,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
-public class EventCategory {
-
-    @Id
-    @GeneratedValue
-    private int id;
+public class EventCategory extends AbstractEntity {
 
     @NotBlank(message = "Name is required")
     @Size(max = 20, min = 2, message = "Name must be between 2 and 20 characters")
@@ -31,7 +27,4 @@ public class EventCategory {
         this.name = name;
     }
 
-    public int getId() {
-        return id;
-    }
 }
